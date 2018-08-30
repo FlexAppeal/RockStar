f57c32e16e41315a6ec9451e86c5dde182c2ac89
Joannis Orlandos <joannis@orlandos.nl>
2018-08-31 01:40:34 +0200

    Temporarily comment BindableCollection, try it out first

--- Sources/Rockstar/Reactivity/BindableCollectoin.swift
+++ Sources/Rockstar/Reactivity/BindableCollectoin.swift
@@ -1,27 +1,27 @@
-public final class BindableCollection<Element>: Collection, ExpressibleByArrayLiteral {
-    let storage: [Binding<Element>]
-    
-    public var startIndex: Int {
-        return storage.startIndex
-    }
-    
-    public var endIndex: Int {
-        return storage.endIndex
-    }
-    
-    public init(arrayLiteral elements: Element...) {
-        self.storage = elements.map(Binding.init)
-    }
-    
-    public init() {
-        self.storage = []
-    }
-    
-    public subscript(index: Int) -> Binding<Element> {
-        return self.storage[index]
-    }
-    
-    public func index(after i: Int) -> Int {
-        return i + 1
-    }
-}
+//public final class BindableCollection<Element>: Collection, ExpressibleByArrayLiteral {
+//    let storage: [Binding<Element>]
+//    
+//    public var startIndex: Int {
+//        return storage.startIndex
+//    }
+//    
+//    public var endIndex: Int {
+//        return storage.endIndex
+//    }
+//    
+//    public init(arrayLiteral elements: Element...) {
+//        self.storage = elements.map(Binding.init)
+//    }
+//    
+//    public init() {
+//        self.storage = []
+//    }
+//    
+//    public subscript(index: Int) -> Binding<Element> {
+//        return self.storage[index]
+//    }
+//    
+//    public func index(after i: Int) -> Int {
+//        return i + 1
+//    }
+//}
