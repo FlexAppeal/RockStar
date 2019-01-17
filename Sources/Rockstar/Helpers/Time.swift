12406d0e89d5ea18636d2e322028799985d76967
Joannis Orlandos <joannis@orlandos.nl>
2019-01-17 11:38:12 +0100

    Improve support for the iPhone 5 (32-bits)

--- Sources/Rockstar/Helpers/Time.swift
+++ Sources/Rockstar/Helpers/Time.swift
@@ -2,26 +2,52 @@ import Foundation
 import Dispatch
 
 public struct RSTimeInterval {
-    public internal(set) var nanoseconds: Int
-    
-    public var dispatch: DispatchTimeInterval {
-        return DispatchTimeInterval.nanoseconds(nanoseconds)
+    public var nanoseconds: Int? {
+        switch dispatch {
+        case .seconds(let s):
+            return s * 1_000_000_000
+        case .milliseconds(let ms):
+            return ms * 1_000_000
+        case .microseconds(let ms):
+            return ms * 1_000
+        case .nanoseconds(let ns):
+            return ns
+        case .never:
+            return nil
+        }
     }
     
+    public var milliseconds: Int? {
+        switch dispatch {
+        case .seconds(let s):
+            return s * 1_000
+        case .milliseconds(let ms):
+            return ms
+        case .microseconds(let ms):
+            return ms / 1_000
+        case .nanoseconds(let ns):
+            return ns / 1_000_000
+        case .never:
+            return nil
+        }
+    }
+    
+    public let dispatch: DispatchTimeInterval
+    
     public static func seconds(_ seconds: Int) -> RSTimeInterval {
-        return .nanoseconds(seconds * 1_000_000_000)
+        return RSTimeInterval(dispatch: .seconds(seconds))
     }
     
     public static func milliseconds(_ ms: Int) -> RSTimeInterval {
-        return .nanoseconds(ms * 1_000_000)
+        return RSTimeInterval(dispatch: .milliseconds(ms))
     }
     
     public static func microseconds(_ ms: Int) -> RSTimeInterval {
-        return .nanoseconds(ms * 1_000)
+        return RSTimeInterval(dispatch: .microseconds(ms))
     }
     
     public static func nanoseconds(_ ns: Int) -> RSTimeInterval {
-        return RSTimeInterval(nanoseconds: ns)
+        return RSTimeInterval(dispatch: .nanoseconds(ns))
     }
     
     public static func seconds(_ seconds: Double) -> RSTimeInterval {
@@ -31,9 +57,9 @@ public struct RSTimeInterval {
     public static func dispatch(_ interval: DispatchTimeInterval) -> RSTimeInterval {
         switch interval {
         case .seconds(let s):
-            return .nanoseconds(s * 1_000_000_000)
+            return .milliseconds(s * 1_000)
         case .milliseconds(let ms):
-            return .nanoseconds(ms * 1_000_000)
+            return .milliseconds(ms)
         case .microseconds(let ms):
             return .nanoseconds(ms * 1_000)
         case .nanoseconds(let ns):
